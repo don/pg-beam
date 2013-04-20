164652ad0d02ad4a27bae0e00974d9eda7e0876b
Don Coleman <dcoleman@chariotsolutions.com>
2013-04-20 14:11:42 -0400

    beam a text file. (beam logo does not work)

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -8,6 +8,9 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
         cameraLabel.addEventListener('touchstart', app.takePicture, false);
+        beamTextLabel.addEventListener('touchstart', app.beamText, false);
+        beamLogoLabel.addEventListener('touchstart', app.beamLogo, false);
+        app.writeSampleTextFile();
         //nfc.addNdefListener(app.onndef);
     },
     // Update DOM on a Received Event
@@ -29,6 +32,7 @@ var app = {
             function(imageURI) { // success
                 //image.src = imageURI;
                 app.beam(imageURI);
+                alert("Tap another Android phone to beam photo.");
             },
             function(error) { // failure
                 console.log("error");
@@ -43,11 +47,50 @@ var app = {
             }
         );
     },
+    beamText: function () {
+        nfc.beam(
+            // Doesn't work "file:///android_asset/www/index.html",
+            "file:///mnt/sdcard/foo.txt",
+            function() {console.log("sharing"); },
+            function(error) { alert(JSON.stringify(error)); }
+        );
+    },
+    beamLogo: function () {
+        // This fails and crashes bluetooth.  I'm guessing the because bluetooth can't access the file uri?
+        nfc.beam(
+            "file:///android_asset/www/img/logo.png",
+            function() {console.log("sharing"); },
+            function(error) { alert(JSON.stringify(error)); }
+        );
+    },
     beam: function (imageURI) {
         nfc.beam(
             imageURI,
             function() {console.log("sharing"); },
             function(error) { alert(JSON.stringify(error)); }
         );
+    },
+    writeSampleTextFile: function () {
+
+        // write a sample text file to /mnt/sdcard/foo.txt
+        // http://docs.phonegap.com/en/2.6.0/cordova_file_file.md.html#FileWriter
+
+        function gotFS(fileSystem) {
+            fileSystem.root.getFile("foo.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+        }
+
+        function gotFileEntry(fileEntry) {
+            fileEntry.createWriter(gotFileWriter, fail);
+        }
+
+        function gotFileWriter(writer) {
+            writer.write("This is a sample file for nfc beam demo.\n");
+        }
+
+        function fail(error) {
+            console.log(error.code);
+        }
+
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
     }
 };
